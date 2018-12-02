ccec23666da38e037f44f0ff0aa7292328ad905a
varun-jindal12 <varun.jindal@sjsu.edu>
2018-12-02 02:46:46 -0800

    completed the update and delete api for user module

--- User/go-burger-user/src/users/server.go
+++ User/go-burger-user/src/users/server.go
@@ -30,7 +30,7 @@ func GetUser(w http.ResponseWriter, req *http.Request) {
     defer session.Close()
     session.SetMode(mgo.Monotonic, true)
     c := session.DB(mongodb_database).C(mongodb_collection)
-    query := bson.M{"Id" : params["id"]}
+    query := bson.M{"id" : params["id"]}
     var result bson.M
     err = c.Find(query).One(&result)
     if err != nil {
@@ -42,7 +42,6 @@ func GetUser(w http.ResponseWriter, req *http.Request) {
 
 func GetAllUser(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	// fmt.Println("Get data of user: ", params["id"])
 	session, err := mgo.Dial(mongodb_server)
     if err != nil {
            panic(err)
@@ -57,14 +56,13 @@ func GetAllUser(w http.ResponseWriter, req *http.Request) {
             log.Fatal(err)
     }
 	fmt.Println("User:", result )
-	//
 	json.NewEncoder(w).Encode(result)
 }
 
 func CreateUser(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	var person User
-	//
 	_ = json.NewDecoder(req.Body).Decode(&person)
 	unqueId := uuid.Must(uuid.NewV4())
 	person.Id = unqueId.String()
@@ -79,31 +77,50 @@ func CreateUser(w http.ResponseWriter, req *http.Request) {
     if err != nil {
             log.Fatal(err)
     }
-	_ = json.NewDecoder(req.Body).Decode(&person)
 	json.NewEncoder(w).Encode(person)
 }
 
 func DeleteUser(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(req)
-	for index, item := range people {
-		if item.Id == params["id"] {
-			people = append(people[:index], people[index+1:]...)
-			break
-		}
-	}
+	session, err := mgo.Dial(mongodb_server)
+    if err != nil {
+           panic(err)
+    }
+    defer session.Close()
+    session.SetMode(mgo.Monotonic, true)
+	c := session.DB(mongodb_database).C(mongodb_collection)
+	query := bson.M{"id":params["Id"]}
+    err = c.Remove(query)
+    if err != nil {
+            log.Fatal(err)
+    }
 	json.NewEncoder(w).Encode(people)
 }
 func UpdateUser(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var person User
+	_ = json.NewDecoder(req.Body).Decode(&person)
 	params := mux.Vars(req)
-	for index, item := range people {
-		if item.Id == params["id"] {
-			people = append(people[:index], people[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(people)
+	session, err := mgo.Dial(mongodb_server)
+    if err != nil {
+           panic(err)
+    }
+    defer session.Close()
+    session.SetMode(mgo.Monotonic, true)
+	c := session.DB(mongodb_database).C(mongodb_collection)
+	query := bson.M{"id":params["id"]}
+	updator := bson.M{
+				"$set": bson.M{
+						"firstname": person.Firstname,
+						"lastname":person.Lastname,
+						"address":person.Address,
+						"password":person.Password}}
+    err = c.Update(query, updator)
+    if err != nil {
+            log.Fatal(err)
+    }
+	json.NewEncoder(w).Encode(updator)
 }
 func UserSignIn(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -119,8 +136,6 @@ func UserSignIn(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
-	// people = append(people, Person{Id: "1", Firstname: "varun", Lastname: "jindal", Address: &Address{Street:"350 e taylo street",City: "Delhi", State: "Delhi",Zipcode:"95112"}, Email:"varun.jindal@sjus.edu", Password:"varun"})
-	// people = append(people, Person{Id: "2", Firstname: "Manali", Lastname: "Jain"})
 	router.HandleFunc("/users", GetAllUser).Methods("GET")
 	router.HandleFunc("/users/{id}", GetUser).Methods("GET")
 	router.HandleFunc("/users/signup", CreateUser).Methods("POST")
