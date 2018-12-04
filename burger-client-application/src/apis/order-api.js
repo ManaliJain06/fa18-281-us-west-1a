5bf582343dff7c0cadefb0499707da8ce82465a0
ariman01 <arijit.mandal@sjsu.edu>
2018-12-04 15:00:53 -0800

    Adding order reducer actions

--- burger-client-application/src/apis/order-api.js
+++ burger-client-application/src/apis/order-api.js
@@ -1,3 +1,34 @@
 /*
 	Client REST API calls to order microservice
 */
+
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://54.153.121.217:8000';
+const headers = {
+    'Accept': 'application/json'
+};
+
+export const getOrderItems = (orderId) =>
+    fetch(`${api}/order/${orderId}`, {
+        method: 'GET',
+        headers: headers,
+    }).then(res => {
+        return res;
+    }).catch(error => {
+        console.log("[order-api] getOrderItems() Error !!!");
+        return error;
+    });
+
+export const  addOrderItem= (menuItem) =>
+    fetch(`${api}/order`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(menuItem)
+    }).then(res => {
+        return res;
+    }).catch(error => {
+        console.log("This is error");
+        return error;
+    });
