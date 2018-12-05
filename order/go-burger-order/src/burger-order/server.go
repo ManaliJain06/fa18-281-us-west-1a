049c304dc243809156ddc0ea0331f4017ace89b3
fjong3 <fulbert.j@gmail.com>
2018-12-05 12:04:52 -0800

    Added Delete Order API

--- order/go-burger-order/src/burger-order/server.go
+++ order/go-burger-order/src/burger-order/server.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
-	"github.com/satori/go.uuid"
 	"github.com/unrolled/render"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -69,7 +68,8 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/order/{orderId}", burgerOrderStatus(formatter)).Methods("GET")
 	mx.HandleFunc("/order", burgerOrderHandler(formatter)).Methods("POST")
 	mx.HandleFunc("/order/{orderId}", burgerOrderPaid(formatter)).Methods("PUT")
-	mx.HandleFunc("/order/{orderId}", burgerOrderDelete(formatter)).Methods("DELETE")
+	mx.HandleFunc("/order/{orderId}", burgerItemDelete(formatter)).Methods("DELETE")
+	mx.HandleFunc("/order", burgerOrderDelete(formatter)).Methods("DELETE")
 }
 
 // Helper Functions
@@ -80,10 +80,20 @@ func failOnError(err error, msg string) {
 	}
 }
 
+func getIp() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+		return "" 
+	}
+     defer conn.Close()
+	 localAddr := conn.LocalAddr().(*net.UDPAddr).String()
+	 return localAddr
+}
+
 // API Ping Handler
 func pingHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		formatter.JSON(w, http.StatusOK, struct{ Test string }{"burger-order API is up!" })
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"burger-order API is up! " + getIp()})
 	}
 }
 
@@ -136,7 +146,6 @@ func burgerOrderHandler(formatter *render.Render) http.HandlerFunc {
 		defer session.Close()
 		session.SetMode(mgo.Monotonic, true)		
 		c := session.DB(mongodb_database).C(mongodb_collection)
-		uuid2, _ := uuid.NewV4()
 		var order BurgerOrder
 		var newitem Items
 		// Find order if it exist
@@ -149,19 +158,18 @@ func burgerOrderHandler(formatter *render.Render) http.HandlerFunc {
 			order.Cart = append(order.Cart, newitem)
 			order.TotalAmount = (order.TotalAmount + newitem.Price)
 			fmt.Println("Orders: ", "Orders found")	
-			c.Update(bson.M{"orderId": orderdetail.OrderId}, bson.M{"$set": bson.M{"items": order.Cart}})
-			c.Update(bson.M{"orderId": orderdetail.OrderId}, bson.M{"$set": bson.M{"totalAmount": order.TotalAmount}})
+			c.Update(bson.M{"orderId": orderdetail.OrderId}, bson.M{"$set": bson.M{"items": order.Cart, "totalAmount": order.TotalAmount, "ipaddress": getIp()}})
 		}else {
 				fmt.Println("Orders: ", "Orders not found")	
 				order = BurgerOrder{
 				OrderId:     orderdetail.OrderId,
-				UserId:      uuid2.String(),
+				UserId:      orderdetail.UserId,
 				OrderStatus: "Placed",
 				TotalAmount: newitem.Price,
 				Cart: []Items{
 					newitem,
 				},
-				IpAddress:	  order.IpAddress, 
+				IpAddress:	  getIp(), 
 			}
 			_ = json.NewDecoder(req.Body).Decode(&order)
 			err = c.Insert(order)
@@ -174,8 +182,41 @@ func burgerOrderHandler(formatter *render.Render) http.HandlerFunc {
 		formatter.JSON(w, http.StatusOK, order)
 	}
 }
-// API Delete Burger Order
-func burgerOrderDelete(formatter *render.Render) http.HandlerFunc {
+
+// API Paid Order
+	func burgerOrderPaid(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		var paymentdetail RequiredPayload
+		_ = json.NewDecoder(req.Body).Decode(&paymentdetail)
+		session, _ := mgo.Dial(mongodb_server)
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		err:= session.DB("admin").Login(mongo_user, mongo_pass)
+		if err!=nil{
+			formatter.JSON(w, http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+		c := session.DB(mongodb_database).C(mongodb_collection)
+		params := mux.Vars(req)
+		var uuid string = params["orderId"]
+		fmt.Println(uuid) 
+		var orderpaid BurgerOrder
+		err = c.Find(bson.M{"orderId": uuid}).One(&orderpaid)
+        if err != nil {
+			fmt.Println("Order not found") 
+			formatter.JSON(w, http.StatusNotFound, "Order Not Found")
+			return
+        }
+		orderpaid.OrderStatus = "Paid"
+		orderpaid.UserId = paymentdetail.UserId
+		c.Update(bson.M{"orderId": uuid}, bson.M{"$set": bson.M{"orderStatus" : orderpaid.OrderStatus, "userId" : orderpaid.UserId, "ipaddress" : getIp()}})
+        fmt.Println("Order:", uuid, "paid" )
+		formatter.JSON(w, http.StatusOK, orderpaid)
+	} 
+} 
+
+// API Delete Item from Order
+func burgerItemDelete(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		session, err := mgo.Dial(mongodb_server)
 		if err != nil {
@@ -208,218 +249,33 @@ func burgerOrderDelete(formatter *render.Render) http.HandlerFunc {
 				break
 			}
 		}
-		c.Update(bson.M{"orderId": uuid}, bson.M{"$set": bson.M{"items": result.Cart}})
-		c.Update(bson.M{"orderId": uuid}, bson.M{"$set": bson.M{"totalAmount": result.TotalAmount}})
+		c.Update(bson.M{"orderId": uuid}, bson.M{"$set": bson.M{"items": result.Cart, "totalAmount": result.TotalAmount, "ipaddress": getIp()}})
 		fmt.Println("Delete Item: ", orderdetail.ItemId, "from order", uuid)
 		formatter.JSON(w, http.StatusOK, result)
 	}
 }
 
-
-// API Paid Order
-	func burgerOrderPaid(formatter *render.Render) http.HandlerFunc {
+// API Delete Burger Order
+func burgerOrderDelete(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		session, _ := mgo.Dial(mongodb_server)
+		session, err := mgo.Dial(mongodb_server)
 		defer session.Close()
-		session.SetMode(mgo.Monotonic, true)
-		err:= session.DB("admin").Login(mongo_user, mongo_pass)
-		if err!=nil{
-			formatter.JSON(w, http.StatusInternalServerError, "Order Not Found")
+		if err:= session.DB("admin").Login(mongo_user, mongo_pass); err != nil {
+			formatter.JSON(w, http.StatusInternalServerError, "Internal Server Error")
 			return
-		}
+		  }
+		session.SetMode(mgo.Monotonic, true)
 		c := session.DB(mongodb_database).C(mongodb_collection)
-		params := mux.Vars(req)
-		var uuid string = params["orderId"]
-		fmt.Println(uuid) 
-		var orderpaid BurgerOrder
-		err = c.Find(bson.M{"orderId": uuid}).One(&orderpaid)
-        if err != nil {
-			fmt.Println("Order not found") 
+		var orderdetail RequiredPayload
+		_ = json.NewDecoder(req.Body).Decode(&orderdetail)
+		fmt.Println("order ID: ", orderdetail.OrderId)
+		err = c.Remove(bson.M{"orderId": orderdetail.OrderId})
+		if err!=nil{
+			fmt.Println("order not found")
 			formatter.JSON(w, http.StatusNotFound, "Order Not Found")
 			return
-        }
-		orderpaid.OrderStatus = "Paid"
-		c.Update(bson.M{"orderId": uuid}, bson.M{"$set": bson.M{"orderStatus" : orderpaid.OrderStatus}})
-        fmt.Println("Order:", uuid, "paid" )
-		formatter.JSON(w, http.StatusOK, orderpaid)
+		}
+		formatter.JSON(w, http.StatusOK, "Order: " + orderdetail.OrderId + " Deleted")
 	} 
 } 
 
-// API Process Orders
-/* func gumballProcessOrdersHandler(formatter *render.Render) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-
-		// Open MongoDB Session
-		session, err := mgo.Dial(mongodb_server)
-        if err != nil {
-                panic(err)
-        }
-        defer session.Close()
-        session.SetMode(mgo.Monotonic, true)
-        c := session.DB(mongodb_database).C(mongodb_collection)
-
-       	// Get Gumball Inventory
-        var result bson.M
-        err = c.Find(bson.M{"SerialNumber" : "1234998871109"}).One(&result)
-        if err != nil {
-                log.Fatal(err)
-        }
-
- 		var count int = result["CountGumballs"].(int)
-        fmt.Println("Current Inventory:", count )
-
-		// Process Order IDs from Queue
-		var order_ids []string = queue_receive()
-		for i := 0; i < len(order_ids); i++ {
-			var order_id = order_ids[i]
-			fmt.Println("Order ID:", order_id)
-			var ord = orders[order_id]
-			ord.OrderStatus = "Order Processed"
-			orders[order_id] = ord
-			count -= 1
-		}
-		fmt.Println( "Orders: ", orders , "New Inventory: ", count)
-
-		// Update Gumball Inventory
-		query := bson.M{"SerialNumber" : "1234998871109"}
-        change := bson.M{"$set": bson.M{ "CountGumballs" : count}}
-        err = c.Update(query, change)
-        if err != nil {
-                log.Fatal(err)
-        }
-
-		// Return Order Status
-		formatter.JSON(w, http.StatusOK, orders)
-	}
-} */
-
-// Send Order to Queue for Processing
-/* func queue_send(message string) {
-	conn, err := amqp.Dial("amqp://"+rabbitmq_user+":"+rabbitmq_pass+"@"+rabbitmq_server+":"+rabbitmq_port+"/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		rabbitmq_queue, // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	body := message
-	err = ch.Publish(
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	log.Printf(" [x] Sent %s", body)
-	failOnError(err, "Failed to publish a message")
-}
-*/
-// Receive Order from Queue to Process
-/* func queue_receive() []string {
-	conn, err := amqp.Dial("amqp://"+rabbitmq_user+":"+rabbitmq_pass+"@"+rabbitmq_server+":"+rabbitmq_port+"/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		rabbitmq_queue, // name
-		false,   // durable
-		false,   // delete when usused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"orders",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	order_ids := make(chan string)
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-			order_ids <- string(d.Body)
-		}
-		close(order_ids)
-	}()
-
-	err = ch.Cancel("orders", false)
-	if err != nil {
-	    log.Fatalf("basic.cancel: %v", err)
-	}
-
-	var order_ids_array []string
-	for n := range order_ids {
-    	order_ids_array = append(order_ids_array, n)
-    }
-
-    return order_ids_array
-} */
-
-/*
-
-	-- Gumball MongoDB Create Database
-
-		Database Name: cmpe281
-		Collection Name: gumball
-
-  	-- Gumball MongoDB Collection (Create Document) --
-
-    db.order.insert(
-	    {
-	      OrderId: "testorder1",
-	      UserId: "testUser1",
-	      OrderStatus: 'Test',
-	      TotalAmount: NumberInt(10)
-	    }
-	) ;
-
-    -- Gumball MongoDB Collection - Find Gumball Document --
-
-    db.gumball.find( { Id: 1 } ) ;
-
-    {
-        "_id" : ObjectId("54741c01fa0bd1f1cdf71312"),
-        "Id" : 1,
-        "CountGumballs" : 202,
-        "ModelNumber" : "M102988",
-        "SerialNumber" : "1234998871109"
-    }
-
-    -- Gumball MongoDB Collection - Update Gumball Document --
-
-    db.gumball.update(
-        { Dd: 1 },
-        { $set : { CountGumballs : NumberInt(10) } },
-        { multi : false }
-    )
-
-    -- Gumball Delete Documents
-
-    db.gumball.remove({})
-
-*/
