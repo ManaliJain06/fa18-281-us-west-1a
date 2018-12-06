dd247d326d6dac044ac796484e3aa273932abf66
varun-jindal12 <varun.jindal@sjsu.edu>
2018-12-05 21:07:07 -0800

    added default user url

--- burger-client-application/src/actions/urlConstant.js
+++ burger-client-application/src/actions/urlConstant.js
@@ -2,4 +2,4 @@
 //export const paymentUrl = 'http://EC2Co-EcsEl-1CNG22VMRNP2G-342756940.us-west-1.elb.amazonaws.com:8000'; // running on AWS ECS
 export const paymentUrl = 'http://35.247.121.55:8000'; // running on Google GKE
 export const orderUrl = 'http://54.153.121.217:8000';
-
+export const userUrl = 'http://EC2Co-EcsEl-TXPKT9YTJTZN-1941324801.us-west-1.elb.amazonaws.com:8000';
