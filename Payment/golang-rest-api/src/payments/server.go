1e7fe3726de98c9f54001827bf767428d4e992a6
nerdijoe <rudy.wahjudi@gmail.com>
2018-12-06 21:21:33 -0800

    updated how to run documentation and clean some comments on payment server.go

--- Payment/golang-rest-api/src/payments/server.go
+++ Payment/golang-rest-api/src/payments/server.go
@@ -36,7 +36,7 @@
  // var mongodb_database = "test" // cmpe281
  // var mongodb_collection = "payments"
  
- // Use EC2 MongoDB Sharding
+ // Use EC2 MongoDB Sharding, get values from environment variables
  var mongodb_server = os.Getenv("AWS_MONGODB")
  var mongodb_database = os.Getenv("MONGODB_DBNAME")
  var mongodb_collection = os.Getenv("MONGODB_COLLECTION")
@@ -55,7 +55,7 @@
 	 n := negroni.Classic()
 	 mx := mux.NewRouter()
 	 initRoutes(mx, formatter)
-	 // n.UseHandler(mx)
+
 	 allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	 allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
 	 allowedOrigins := handlers.AllowedOrigins([]string{"*"})
@@ -77,23 +77,13 @@
 	 mx.HandleFunc("/payments", createPayments(formatter)).Methods("POST")
 	 mx.HandleFunc("/payments/{id}", deletePayment(formatter)).Methods("DELETE")
 	 mx.HandleFunc("/payments/{id}", editPayment(formatter)).Methods("PUT")
- 
  }
  
  func handleRequest() {
  
-	 // myRouter := mux.NewRouter().StrictSlash(true)
 	 t := time.Now()
 	 payments = append(payments, Payment{PaymentID: "1", UserID: "1", OrderID: "11", TotalAmount: 100.50, Status: true, PaymentDate: t.Format("2006-01-02 15:04:05")})
 	 payments = append(payments, Payment{PaymentID: "2", UserID: "2", OrderID: "22", TotalAmount: 30.30, Status: true, PaymentDate: t.Format("2006-01-02 15:04:05")})
- 
-	 // myRouter.HandleFunc("/", homePage)
-	 // myRouter.HandleFunc("/payments", allPayments).Methods("GET")
-	 // myRouter.HandleFunc("/payments/{id}", getPaymentById).Methods("GET")
-	 // myRouter.HandleFunc("/payments", createPayments).Methods("POST")
-	 // myRouter.HandleFunc("/payments/{id}", deletePayment).Methods("DELETE")
- 
-	 // log.Fatal(http.ListenAndServe(":8081", myRouter))
  }
  
  // API Ping Handler
@@ -115,29 +105,12 @@
 	 fmt.Println("address: ", address[0])
 	 return address[0]
  }
- 
- // func allPayments(w http.ResponseWriter, r *http.Request) {
- // t := time.Now()
- // payments := Payments{
- // 	Payment{paymentId: "100", userId: "1", orderId: "33", totalAmount: 100.50, status: true, paymentDate: t.Format("2006-01-02 15:04:05")},
- // }
- 
- // payments := Payments{
- // 	Payment{PaymentID: "100", UserID: "1", OrderID: "33", TotalAmount: 100.50, Status: true, PaymentDate: t.Format("2006-01-02 15:04:05")},
- // }
- 
- // 	fmt.Println("Get all payments")
- 
- // 	fmt.Println(payments)
- // 	json.NewEncoder(w).Encode(payments)
- // }
- 
+  
  func getAllPayments(formatter *render.Render) http.HandlerFunc {
 	 return func(w http.ResponseWriter, req *http.Request) {
 		 session, _ := mgo.Dial(mongodb_server)
 		 err := session.DB("admin").Login(mongodb_username, mongodb_password)
 		 if err != nil {
-			 // panic(err)
 			 formatter.JSON(w, http.StatusInternalServerError, "Mongo Connection Error")
 			 return
 		 }
@@ -147,7 +120,6 @@
 		 var result []bson.M
 		 err = c.Find(nil).All(&result)
 		 if err != nil {
-			 // log.Fatal(err)
 			 formatter.JSON(w, http.StatusNotFound, "Get All Payment Error")
 			 return
 		 }
@@ -161,7 +133,6 @@
 		 session, _ := mgo.Dial(mongodb_server)
 		 err := session.DB("admin").Login(mongodb_username, mongodb_password)
 		 if err != nil {
-			 // panic(err)
 			 formatter.JSON(w, http.StatusInternalServerError, "Mongo Connection Error")
 			 return
 		 }
@@ -175,22 +146,11 @@
  
 		 err = c.Find(bson.M{"paymentid": params["id"]}).One(&result)
 		 if err != nil {
-			 // log.Fatal(err)
 			 formatter.JSON(w, http.StatusNotFound, "Get Payment by ID Error")
 			 return
 		 }
 		 fmt.Println("getPaymentByID:", result)
 		 formatter.JSON(w, http.StatusOK, result)
- 
-		 // fmt.Println("getPaymentByPaymentId")
-		 // params := mux.Vars(req)
-		 // for _, item := range payments {
-		 // 	if item.PaymentID == params["id"] {
-		 // 		// json.NewEncoder(w).Encode(item)
-		 // 		formatter.JSON(w, http.StatusOK, item)
-		 // 		return
-		 // 	}
-		 // }
 	 }
  }
  
@@ -199,14 +159,12 @@
 		 session, _ := mgo.Dial(mongodb_server)
 		 err := session.DB("admin").Login(mongodb_username, mongodb_password)
 		 if err != nil {
-			 // panic(err)
 			 formatter.JSON(w, http.StatusInternalServerError, "Mongo Connection Error")
 			 return
 		 }
 		 defer session.Close()
 		 session.SetMode(mgo.Monotonic, true)
 		 c := session.DB(mongodb_database).C(mongodb_collection)
-		 // var result bson.M
  
 		 var payment Payment
 		 _ = json.NewDecoder(req.Body).Decode(&payment)
@@ -217,32 +175,13 @@
 		 payment.PaymentDate = t.Format("2006-01-02 15:04:05")
 		 payment.Status = true
  
-		 // err = c.Find(bson.M{"SerialNumber" : "1234998871109"}).One(&result)
 		 err = c.Insert(payment)
 		 if err != nil {
-			 // log.Fatal(err)
 			 formatter.JSON(w, http.StatusNotFound, "Create Payment Error")
 			 return
 		 }
 		 fmt.Println("Create new payment:", payment)
 		 formatter.JSON(w, http.StatusOK, payment)
- 
-		 // fmt.Println("createPayments")
-		 // // params := mux.Vars(req)
-		 // var payment Payment
-		 // _ = json.NewDecoder(req.Body).Decode(&payment)
- 
-		 // uuid, _ := uuid.NewV4()
-		 // payment.PaymentID = uuid.String()
-		 // t := time.Now()
-		 // payment.PaymentDate = t.Format("2006-01-02 15:04:05")
-		 // payment.Status = true
- 
-		 // payments = append(payments, payment)
- 
-		 // // fmt.Fprintf(w, "createPayments")
-		 // // json.NewEncoder(w).Encode(payments)
-		 // formatter.JSON(w, http.StatusOK, payments)
 	 }
  }
  
@@ -251,7 +190,6 @@
 		 session, _ := mgo.Dial(mongodb_server)
 		 err := session.DB("admin").Login(mongodb_username, mongodb_password)
 		 if err != nil {
-			 // panic(err)
 			 formatter.JSON(w, http.StatusInternalServerError, "Mongo Connection Error")
 			 return
 		 }
@@ -263,7 +201,6 @@
  
 		 err = c.Find(bson.M{"paymentid": params["id"]}).One(&result)
 		 if err != nil {
-			 // log.Fatal(err)
 			 formatter.JSON(w, http.StatusNotFound, "Delete Payment: cannot find ID Error")
 			 return
 		 } else {
@@ -272,7 +209,6 @@
  
 			 err = c.Remove(bson.M{"paymentid": result.PaymentID})
 			 if err != nil {
-				 // log.Fatal(err)
 				 fmt.Println("error:" + err.Error())
 				 formatter.JSON(w, http.StatusNotFound, "Delete Payment: delete Error")
 				 return
@@ -280,33 +216,19 @@
 		 }
 		 fmt.Println("deletePayment ", result)
 		 formatter.JSON(w, http.StatusOK, result)
- 
-		 // fmt.Println("deletePayment")
-		 // params := mux.Vars(req)
-		 // for index, item := range payments {
-		 // 	if item.PaymentID == params["id"] {
-		 // 		payments = append(payments[:index], payments[index+1:]...)
-		 // 		break
-		 // 	}
-		 // }
- 
-		 // // json.NewEncoder(w).Encode(payments)
-		 // formatter.JSON(w, http.StatusOK, payments)
 	 }
  }
  
  func editPayment(formatter *render.Render) http.HandlerFunc {
 	 return func(w http.ResponseWriter, req *http.Request) {
  
-		 // find payment, edit
- 
+		 // find payment, then edit
 		 var payment Payment
 		 _ = json.NewDecoder(req.Body).Decode(&payment)
 		 fmt.Println("Edit payment to these attributes: ", payment)
 		 session, _ := mgo.Dial(mongodb_server)
 		 err := session.DB("admin").Login(mongodb_username, mongodb_password)
 		 if err != nil {
-			 // panic(err)
 			 formatter.JSON(w, http.StatusInternalServerError, "Mongo Connection Error")
 			 return
 		 }
@@ -318,7 +240,6 @@
 		 var fetchedPayment bson.M
 		 err = c.Find(bson.M{"paymentid": params["id"]}).One(&fetchedPayment)
 		 if err != nil {
-			 // log.Fatal(err)
 			 formatter.JSON(w, http.StatusNotFound, "Edit Payment Error")
 			 return
 		 }
@@ -328,7 +249,6 @@
 		 fetchedPayment["status"] = payment.Status
  
 		 query := bson.M{"paymentid": params["id"]}
-		 // change := bson.M{"$set": bson.M{ "CountGumballs" : m.CountGumballs}}
 		 err = c.Update(query, &fetchedPayment)
 		 if err != nil {
 			 log.Fatal(err)
@@ -336,88 +256,11 @@
  
 		 fmt.Println("Edit Payment:", fetchedPayment)
 		 formatter.JSON(w, http.StatusOK, fetchedPayment)
- 
-		 // fmt.Println("editPayment")
- 
-		 // var payment Payment
-		 // _ = json.NewDecoder(req.Body).Decode(&payment)
- 
-		 // fmt.Println("before", payment)
- 
-		 // params := mux.Vars(req)
-		 // for index, item := range payments {
-		 // 	if item.PaymentID == params["id"] {
-		 // 		payment.PaymentID = item.PaymentID
-		 // 		payment.UserID = item.UserID
-		 // 		payment.OrderID = item.OrderID
-		 // 		payment.PaymentDate = item.PaymentDate
- 
-		 // 		fmt.Println(payment)
- 
-		 // 		payments = append(payments[:index], payments[index+1:]...)
-		 // 		payments = append(payments, payment)
- 
-		 // 		break
-		 // 	}
-		 // }
- 
-		 // // json.NewEncoder(w).Encode(payments)
-		 // formatter.JSON(w, http.StatusOK, payments)
 	 }
  }
  
- // func getPaymentById(w http.ResponseWriter, req *http.Request) {
- // 	fmt.Println("getPaymentByPaymentId")
- // 	params := mux.Vars(req)
- // 	for _, item := range payments {
- // 		if item.PaymentID == params["id"] {
- // 			json.NewEncoder(w).Encode(item)
- // 			return
- // 		}
- // 	}
- 
- // 	// fmt.Fprintf(w, "getPaymentByPaymentId")
- // 	json.NewEncoder(w).Encode(&Payment{})
- // }
- 
- // func createPayments(w http.ResponseWriter, req *http.Request) {
- // 	fmt.Println("createPayments")
- // 	// params := mux.Vars(req)
- // 	var payment Payment
- // 	_ = json.NewDecoder(req.Body).Decode(&payment)
- 
- // 	uuid, _ := uuid.NewV4()
- // 	payment.PaymentID = uuid.String()
- // 	t := time.Now()
- // 	payment.PaymentDate = t.Format("2006-01-02 15:04:05")
- // 	payment.Status = true
- 
- // 	payments = append(payments, payment)
- 
- // 	// fmt.Fprintf(w, "createPayments")
- // 	json.NewEncoder(w).Encode(payments)
- // }
- 
- // func deletePayment(w http.ResponseWriter, req *http.Request) {
- // 	fmt.Println("deletePayment")
- // 	params := mux.Vars(req)
- // 	for index, item := range payments {
- // 		if item.PaymentID == params["id"] {
- // 			payments = append(payments[:index], payments[index+1:]...)
- // 			break
- // 		}
- // 	}
- 
- // 	json.NewEncoder(w).Encode(payments)
- // }
- 
- // func homePage(w http.ResponseWriter, r *http.Request) {
- // 	fmt.Fprintf(w, "Heyyooo")
- // }
- 
- /*
- 
- 
+
+ /* 
 	 db.payments.insert({
 		 PaymentID: '1',
 		 UserID: '1',
